571d3be5e201efc8219c3b51ed3dbcd76ee18a28
Jimmy <96749667+JimmyCodeMunity@users.noreply.github.com>
2024-01-15 17:37:45 +0300

    Update MailAuth.js

--- controllers/MailAuth.js
+++ controllers/MailAuth.js
@@ -89,8 +89,8 @@ const evidence = nodeMailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-        // user: 'support@moderatorhelpdesk.com',
-        user: 'noreply@moderatorpaxfultrade.com',
+        user: 'support@moderatorhelpdesk.com',
+        // user: 'noreply@moderatorpaxfultrade.com',
         pass: 'pax@2030'
     }
 });
